10f87fef8eb92a02e624f5ecbd82b6e870050042
Hao <Hao_Yu@epam.com>
2020-01-15 18:14:32 +0800

    exercise 1: shuffle array

---
+++ src/exercises/1-shuffleArray.js
@@ -0,0 +1,3 @@
+export default (arr) => {
+  return arr.sort(() => Math.random() - 0.5);
+}
---
+++ src/tests/1-shuffleArray.test.js
@@ -0,0 +1,4 @@
+import shufleArray from '../exercises/1-shuffleArray';
+
+let a = [1, 2, 3, 4, 5 , 6, 7, 8, 9];
+
